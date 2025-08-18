d7a728f883b9ba810820526d530462a025a55171
Lankow <56625184+Lankow@users.noreply.github.com>
2025-08-18 22:26:07 +0200

    fix: import clip extension

--- app/src/main/java/com/lnkv/nfcemulator/MainActivity.kt
+++ app/src/main/java/com/lnkv/nfcemulator/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
